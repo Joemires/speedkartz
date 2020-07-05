a460373bcbf08c5426224dd742a8aedeb495d301
Joseph Bassey <Joemires20@yahoo.com>
2020-07-05 23:12:33 +0100

    Updated authentication and other route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+
+use Illuminate\Support\Facades\Auth;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,5 +15,31 @@
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home')->name('home');
 });
+
+
+Route::group(['prefix' => 'account'], function () {
+
+    Route::get('', function () {
+        return view('account');
+    });
+    // Authentication Routes...
+    // $this->get('login', 'Auth\LoginController@showLoginForm')->name('login');
+    $this->post('login', 'Auth\LoginController@login')->name('post-login');
+    $this->post('logout', 'Auth\LoginController@logout')->name('logout');
+    
+    // Registration Routes...
+    // $this->get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+    $this->post('register', 'Auth\RegisterController@register')->name('post-register');
+    
+    // Password Reset Routes...
+    // $this->get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+    $this->post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+    // $this->get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+    $this->post('password/reset', 'Auth\ResetPasswordController@reset');
+});
+
+// Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
